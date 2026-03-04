d269d4ae052b1d50a4bf9a0dda5fb136169f48f5
Yashkunwarji <yashkunwarji@gmail.com>
2026-03-04 15:14:38 +0000

    container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,8 @@
+const c=1
+let a=2
+var b=3
+d=4
+console.table([a,b,c,d])
+for(let i=1;i<=10;i++){
+    console.table(a*i);
+}
---
+++ 01_basics/02_data_types.js
@@ -0,0 +1,13 @@
+"use strict" ;//treat all JS code as newer version
+//alert("hello"); not browser
+console.log("okay"); console.log("time");
+//number =>2 power 53
+//bigint
+//string = " "
+//boolean = true/false
+//null
+//undefined
+//symbol=>unique
+console.log(typeof "yash");//string
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
