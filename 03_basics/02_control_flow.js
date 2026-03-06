0f5ece1d2d813dbcfc1d0c67a29f750303686fc1
Yashkunwarji <yashkunwarji@gmail.com>
2026-03-06 16:52:46 +0000

    container config done

---
+++ 03_basics/02_control_flow.js
@@ -0,0 +1,34 @@
+//if-else statement 
+
+if(2<1){
+    console.log("yes");
+}
+else if(2<3){
+    console.log("maybe");
+}
+else{
+    console.log("no");
+}
+
+//switch case
+
+const mont=3;
+switch(mont){
+    case(1):
+    console.log("jan");
+    break;
+    case(2):
+    console.log("feb");
+    break;
+    case(3):
+    console.log("mar");
+    break;
+    default:
+        console.log('no');
+}
+
+//ternary operator
+
+2>=3? console.log("yes"):console.log("no");
+
+
